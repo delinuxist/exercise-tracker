65a5405e66411cfc8f2def75b82de586e1ec035e
delinuxist <delinuxist@gmail.com>
2021-04-26 14:37:52 +0000

    navbar component file

---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          ExerciseTracker
+        </Link>
+        <button
+          type="button"
+          className="navbar-toggler nav-button"
+          data-toggle="collapse"
+          data-target="#Mynavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="Mynavbar">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Exercises
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/create" className="nav-link">
+                Create Exercise
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/user" className="nav-link">
+                User
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
