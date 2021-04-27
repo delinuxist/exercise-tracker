38ee86d6eec9a6a7b4f28638922bf304ee151863
delinuxist <delinuxist@gmail.com>
2021-04-27 12:30:52 +0000

    fixed problem

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,22 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Exercise = (props) => {
-  const exercises = props.exercise.map((exercise) => {
-    return { exercise };
-  });
-  // const [_id, username, description, duration, date] = props.exercise;
+const Exercise = ({ exercise, deleteExercise }) => {
+  // const exercises = props.exercises.map((exercise) => {
+  //   return { exercise };
+  // });
+  const { _id, username, description, duration, date } = exercise;
   return (
     <tr>
-      <td>{exercises.username}</td>
-      <td>{exercises.description}</td>
-      <td>{exercises.duration}</td>
-      <td>{exercises.date}</td>
+      <td>{username}</td>
+      <td>{description}</td>
+      <td>{duration}</td>
+      <td>{date.substring(0, 10)}</td>
       <td>
-        <Link to={"/edit" + exercises._id}>Edit</Link> |{" "}
+        <Link className="btn btn-dark" to={"/edit"}>
+          Edit
+        </Link>{" "}
+        |{" "}
         <button
-          // onClick={props.deleteexercise(exercises._id)
-          // }
+          // onClick={deleteExercise(_id)}
+
           className="btn btn-danger"
         >
           Delete
